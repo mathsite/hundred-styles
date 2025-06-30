1a5a2b0e1bd24a54c849c6a9bf8b06189133f10f
1.9.8.2.6 (AJM) <74386189+ajm19826@users.noreply.github.com>
2025-06-30 19:22:15 -0400

    script for modal

---
+++ static/script.js
@@ -0,0 +1,12 @@
+  function openModal() {
+    document.getElementById('stylesModal').style.display = 'flex';
+  }
+  function closeModal() {
+    document.getElementById('stylesModal').style.display = 'none';
+  }
+  function copyLink() {
+    const codeText = document.getElementById("cssLinkCode").innerText;
+    navigator.clipboard.writeText(codeText).then(() => {
+      alert("Link copied to clipboard!");
+    });
+  }
